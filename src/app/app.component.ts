50feafa3c7890afdf1871705485092c0ebe62f1c
unknown <khubaibzubairi@outlook.com>
2023-03-28 15:37:04 +0500

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'result-summary';
+
+  ngOnInit(): void {
+    console.log(new Date('Mon 27 Mar 2023 12:29:18 PM UTC').getUTCDate());
+  }
 }
